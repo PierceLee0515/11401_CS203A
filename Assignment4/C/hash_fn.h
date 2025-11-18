863096bb7ad67e4f37d56cf2eed9daceeb2931ab
YI HSIANG LEE <149958668+PierceLee0515@users.noreply.github.com>
2025-11-19 06:26:48 +0800

    Create hash_fn.h for integer and string hashing
    Added header file for hash functions with documentation.

--- Assignment4/C/hash_fn.h
+++ Assignment4/C/hash_fn.h
@@ -1,1 +1,23 @@
+/*
+   ========================================
+   hash_fn.h ??? declare your hash functions
+   ========================================
 
+   Description:
+    This file contains the hash functions for integer and string keys.
+
+   Development History:
+    - 2025/11/18: Initial implementation
+    - 2025/11/19: Finished Development
+   Developer:YI-HSIANG LEE <piercelee0515@gmail.com>
+ */
+#ifndef HASH_FN_H
+#define HASH_FN_H
+
+#include <stdio.h>
+#include <string.h>
+
+int myHashInt(int key, int m);
+int myHashString(const char* str, int m);
+
+#endif
