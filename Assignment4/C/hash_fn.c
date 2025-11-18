7a10435e00405ee9f715aa22f45b8e890f7f21fd
YI HSIANG LEE <149958668+PierceLee0515@users.noreply.github.com>
2025-11-19 03:28:52 +0800

    Update hash_fn.c

--- Assignment4/C/hash_fn.c
+++ Assignment4/C/hash_fn.c
@@ -1,1 +1,23 @@
+#include "hash_fn.h"
+#include <math.h>
+
+int myHashInt(int key, int m) {
+    const double A = 0.6180339;   // slightly more precise constant
+    double value = key * A;
+    double frac  = value - floor(value);
+    return (int)(frac * m);
+}
+
+int myHashString(const char* str, int m) {
+    unsigned long hash = 5381;
+    unsigned char c;
+
+    while ((c = (unsigned char)*str++) != '\0')
+    {
+        hash = (hash << 5) + hash;  // hash * 32 + hash
+        hash += c;                  // += character
+    }
+
+    return (int)(hash % m);
+}
 
